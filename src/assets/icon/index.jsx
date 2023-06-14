3ac04da387f07a9cc6bba20dc61cc5031052ed77
Raymond Nguyen <phuntv.jp@gmail.com>
2023-06-15 04:52:21 +0700

    fix

--- src/assets/icon/index.jsx
+++ src/assets/icon/index.jsx
@@ -1,12 +1,10 @@
-<<<<<<< HEAD
-export { default as LogoIcon} from './Logo';
-export { default as SearchIcon} from './SearchIcon';
-export {default as CartIcon} from './CartIcon';
-export {default as Polygon1} from './Polygon1';
-export {default as Polygon2} from './Polygon2';
-export {default as Heart1} from './Heart1';
-export {default as Heart2} from './Heart2';
-=======
->>>>>>> 76ff8d5e32d16644d37544f9c2a136600fa19c33
+
+export { default as LogoIcon } from './Logo';
+export { default as SearchIcon } from './SearchIcon';
+export { default as CartIcon } from './CartIcon';
+export { default as Polygon1 } from './Polygon1';
+export { default as Polygon2 } from './Polygon2';
+export { default as Heart1 } from './Heart1';
+export { default as Heart2 } from './Heart2';
 export { default as EyeIcon } from './EyeIcon';
 export { default as EyeSlashIcon } from './EyeSlashIcon';
